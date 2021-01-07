48db1f300b4e302d8192cd9210e603d7e392c4e2
Simen Heggestøyl <simen.heggestoyl@origo.oslo.kommune.no>
2021-01-07 10:30:23 +0100

    Bump version: 0.2.0 → 0.3.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
-## ?.?.?
+## 0.3.0
 
 * The `okdata` namespace package now uses the old-style `pkg_resources`
   declaration instead of being an implicit namespace package.
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="okdata-aws",
-    version="0.2.0",
+    version="0.3.0",
     author="Oslo Origo",
     author_email="dataplattform@oslo.kommune.no",
     description="Collection of helpers for working with AWS",
